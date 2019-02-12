cbcb6ac399e3b5f2613150d9206f21f1a7a2d9ca
Marcelo Jr <marcelo.jr63@gmail.com>
2019-02-12 19:19:48 -0200

    Adding score and impact between shots and meteors

--- lib/spacelixir.ex
+++ lib/spacelixir.ex
@@ -14,7 +14,7 @@ defmodule Spacelixir do
   end
 
   def stateInit do
-    %{spacecraft: %{x: 50, y: 50}, shots: [], meteors: [], time: 0}
+    %{spacecraft: %{x: 20, y: 30}, shots: [], meteors: [], time: 0, score: 0}
   end
 
   def loop(state) do
@@ -24,8 +24,8 @@ defmodule Spacelixir do
 
       :tick ->
         schedule_next_tick()
-        draw(state)
         state
+        |> draw()
         |> update()
         |> loop()
     end
@@ -35,23 +35,47 @@ defmodule Spacelixir do
     Process.send_after(self(), :tick, 100)
   end
 
-  defp handle_key(?w, state), do: %{state | spacecraft: %{x: state.spacecraft.x,     y: state.spacecraft.y - 1 }}
-  defp handle_key(?a, state), do: %{state | spacecraft: %{x: state.spacecraft.x - 1, y: state.spacecraft.y     }}
-  defp handle_key(?s, state), do: %{state | spacecraft: %{x: state.spacecraft.x,     y: state.spacecraft.y + 1 }}
-  defp handle_key(?d, state), do: %{state | spacecraft: %{x: state.spacecraft.x + 1, y: state.spacecraft.y     }}
+  defp handle_key(?w, state), do: update_spacecraft(state, state.spacecraft.x,     state.spacecraft.y - 1)
+  defp handle_key(?a, state), do: update_spacecraft(state, state.spacecraft.x - 1, state.spacecraft.y    )
+  defp handle_key(?s, state), do: update_spacecraft(state, state.spacecraft.x,     state.spacecraft.y + 1)
+  defp handle_key(?d, state), do: update_spacecraft(state, state.spacecraft.x + 1, state.spacecraft.y    )
   defp handle_key(?q, _),     do: ExNcurses.endwin()
-
   defp handle_key(?k, state) do
     ExNcurses.beep()
     %{state| shots: state.shots ++ [%{x: state.spacecraft.x+1, y: state.spacecraft.y}]}
   end
+  defp handle_key(_, state),  do: state
+
+  defp update_spacecraft(state, x, y) do
+    if x <= 38 && x >= 0 && y <= 39 && y >= 0 do
+      %{state | spacecraft: %{x: x, y: y}}
+    else
+      state
+    end
+  end
 
   defp draw(state) do
     ExNcurses.clear()
+    draw_borders()
     draw_shots(state.shots)
     draw_meteors(state.meteors)
     draw_spacecraft(state.spacecraft)
+    draw_score(state.score)
     ExNcurses.refresh()
+    state
+  end
+
+  defp draw_borders() do
+    (0..40)
+    |> Enum.each(fn limit ->
+      ExNcurses.mvaddstr(41, limit, "-")
+      ExNcurses.mvaddstr(limit, 41, "|")
+    end)
+  end
+
+  defp draw_score(score) do
+    ExNcurses.mvaddstr(5, 50, "SCORE")
+    ExNcurses.mvaddstr(5, 57, to_string(score))
   end
 
   defp draw_meteors(meteors) do
@@ -83,6 +107,7 @@ defmodule Spacelixir do
     |> update_time()
     |> add_meteor()
     |> update_shots()
+    |> destroy_meteors()
     |> update_meteors()
   end
 
@@ -91,28 +116,65 @@ defmodule Spacelixir do
   end
 
   defp update_meteors(state) do
-    %{state | meteors: generate_meteors_arr(state.meteors)}
+    if rem(state.time, 5) == 0 do
+      %{state | meteors: generate_meteors_list(state.meteors)}
+    else
+      state
+    end
   end
 
-  def add_meteor(state) do
-    if rem(state.time, 10) == 0 do
-      %{state | meteors: state.meteors ++ [%{x: :rand.uniform(40), y: 1}]}
+  defp destroy_meteors(state) do
+    %{state | meteors: valid_meteors_list(state), shots: valid_shots_list(state), score: update_score(state)}
+  end
+
+  defp update_score(state) do
+    if had_destroyed_meteor?(state), do: state.score + 1, else: state.score
+  end
+
+  defp had_destroyed_meteor?(state) do
+    state.meteors
+    |> Enum.any?(fn meteor ->
+      Enum.any?(state.shots, fn shot -> meteor_was_destroyed?(meteor, shot) end)
+    end)
+  end
+
+  defp valid_meteors_list(state) do
+    state.meteors
+    |> Enum.filter(fn meteor ->
+      not Enum.any?(state.shots, fn shot -> meteor_was_destroyed?(meteor, shot) end)
+    end)
+  end
+
+  defp valid_shots_list(state) do
+    state.shots
+    |> Enum.filter(fn shot ->
+      not Enum.any?(state.meteors, fn meteor -> meteor_was_destroyed?(meteor, shot) end)
+    end)
+  end
+
+  defp meteor_was_destroyed?(meteor, shot) do
+    (meteor.x <= shot.x && shot.x <= meteor.x+2) && (meteor.y <= shot.y && shot.y <= meteor.y+1)
+  end
+
+  defp add_meteor(state) do
+    if rem(state.time, 20) == 0 do
+      %{state | meteors: state.meteors ++ [%{x: :rand.uniform(37), y: 1}]}
     else
       state
     end
   end
 
   defp update_shots(state) do
-    %{state | shots: generate_shots_arr(state.shots)}
+    %{state | shots: generate_shots_list(state.shots)}
   end
 
-  defp generate_shots_arr(shots) do
+  defp generate_shots_list(shots) do
     shots
     |> Enum.map(fn shot -> %{x: shot.x, y: shot.y - 1} end)
     |> Enum.filter(fn shot -> shot.x >= 0 && shot.y >= 0 end)
   end
 
-  defp generate_meteors_arr(meteors) do
+  defp generate_meteors_list(meteors) do
     meteors
     |> Enum.map(fn meteor -> %{x: meteor.x, y: meteor.y + 1} end)
     |> Enum.filter(fn meteor -> meteor.y < 50 end)
